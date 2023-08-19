69cfd9ef0669e8f40ae6a80016918bb859e3fed9
haohao3945 <haohao3945@gmail.com>
2023-08-19 11:05:18 +0800

    Added basic crm functionality

---
+++ crmapp/crmapplication/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Contact
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'crm/contact_list.html', {'contacts': contacts})
