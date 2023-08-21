b5feeb4eebe3c61345cc1e228587dd6c33bd7b5a
haohao3945 <haohao3945@gmail.com>
2023-08-21 12:15:59 +0800

    refine the search engine

--- crmapp/crmapplication/urls.py
+++ crmapp/crmapplication/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('order_data/', views.order_data, name='order_data'),
     path('customer_detail/', views.customer_detail, name='customer_detail'),
+    path('contact_search/', views.contact_search, name='contact_search'),
+    path('order_search/', views.order_search, name='order_search'),
+
 ]
--- crmapp/crmapplication/views.py
+++ crmapp/crmapplication/views.py
@@ -2,6 +2,8 @@ from django.core.paginator import Paginator, Page
 from django.shortcuts import render
 #from .forms import CustomerForm  # Import your customer form
 from .models import Customer,Invoice
+from django.db.models import Q
+
 
 def contact_list(request):
     contacts = Customer.objects.all()
@@ -10,6 +12,18 @@ def contact_list(request):
     contacts = paginator.get_page(page)
     
     return render(request, 'crm/contact_list.html', {'contacts': contacts})
+
+
+def contact_search(request):
+    query = request.GET.get('q')
+    if query:
+        contacts = Customer.objects.filter(
+            Q(customer_id__contains=query) | Q(first_name__contains=query) | Q(last_name__contains=query) | Q(email__contains=query) | Q(phone__contains=query) | Q(progress__contains=query)| Q(address__contains=query)
+        )
+    else:
+        contacts = Customer.objects.all()
+    return render(request, 'crm/contact_list.html', {'contacts': contacts})
+
 
 def dashboard(request):
     return render(request, 'crm/dashboard.html')
@@ -20,8 +34,53 @@ def order_data(request):
     page = request.GET.get('page')
     order = paginator.get_page(page)
     return render(request, 'crm/order_data.html',{'orders':order})
+
+MONTH_MAP = {
+    'jan': '01',
+    'feb': '02',
+    'mar': '03',
+    'apr': '04',
+    'may': '05',
+    'jun': '06',
+    'jul': '07',
+    'aug': '08',
+    'sep': '09',
+    'sept': '09',
+    'oct': '10',
+    'nov': '11',
+    'dec': '12',
+}
+
+def normalize_query(query):
+    # Normalize month search query to numeric format if it's an English month name
+    normalized_query = query.lower()
+    if normalized_query in MONTH_MAP:
+        return MONTH_MAP[normalized_query]
+    return None
+    
+def order_search(request):
+    query = request.GET.get('q')
+
+    if query:
+        normalized_query = normalize_query(query)
+        
+        if normalized_query in MONTH_MAP.values():         
+            # Search for invoices by month
+            order = Invoice.objects.filter(invoice_date__month=normalized_query)
+        else:
+            # Perform the regular search
+            order = Invoice.objects.filter(
+                Q(invoice_id__contains=query) |
+                Q(invoice_date__icontains=query) |
+                Q(customer__first_name__exact=query) |
+                Q(customer__last_name__exact=query) |
+                Q(country__contains=query) |
+                Q(notice_to_consider__contains=query)
+            )
+    return render(request, 'crm/order_data.html', {'orders': order})
     
 def customer_detail(request):
+    print("Request - ", request)
  #   if request.method == 'POST':
  #       form = CustomerForm(request.POST)
     
